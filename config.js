9495ff0e6a30070c5a861372e3de7b2a951b8cac
hakisolos <165590443+hakisolos@users.noreply.github.com>
2024-10-24 14:23:28 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PREFIX: process.env.PREFIX || "KING-HAKI",
+  EMAIL: process.env.EMAIL || "maxwellexcel2@gmail.com",
+  PASS: process.env.PASS || "mynameisexcel2",
+  MESSAGE: process.env.MESSAGE || "Thank you for choosing HAKI-MD, hope you have a great time😃❤️",
+  IMAGE: process.env.IMAGE || "https://d.uguu.se/bWuGRJxS.jpg",
+}
